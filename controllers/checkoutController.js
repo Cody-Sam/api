5b3c590faad76b3400e296e8f14f3da9da98b640
Cody Partington <codythatsme@gmail.com>
2022-07-15 13:29:44 +1000

    basic cart implementation added

---
+++ controllers/checkoutController.js
@@ -0,0 +1,38 @@
+const asyncHandler = require("express-async-handler");
+const ProductModel = require("../db/models/productModel");
+const { productIndex } = require('./productController')
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE)
+
+const createCheckoutSession = asyncHandler(async (req, res) => {
+  try {
+    const storeItems = await ProductModel.find()
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      mode: "payment",
+      line_items: req.body.items.map(item => {
+        const storeItem = storeItems.find(storeItem => storeItem._id.toString() === item._id)
+        console.log(storeItem)
+        return {
+          price_data: {
+            currency: "aud",
+            product_data: {
+              name: storeItem.name,
+            },
+            unit_amount: storeItem.price,
+          },
+          quantity: item.quantity,
+        }
+      }),
+      success_url: `${process.env.CLIENT_URL}/success.html`,
+      cancel_url: `${process.env.CLIENT_URL}/failure.html`,
+    })
+    res.json({ url: session.url })
+  }
+  catch (err) {
+    console.error(err)
+  }
+})
+
+module.exports = {
+    createCheckoutSession
+}
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const userRoutes = require("./userRoutes");
 const productRoutes = require("./productRoutes");
 const orderRoutes = require("./orderRoutes");
 const buildRoutes = require("./buildRoutes");
+const checkoutRoutes = require('./checkoutRoutes')
 
 router.use("/users", userRoutes);
 router.use("/products", productRoutes);
 router.use("/orders", orderRoutes);
 router.use("/builds", buildRoutes);
+router.use("/checkout", checkoutRoutes)
 
 module.exports = router;
