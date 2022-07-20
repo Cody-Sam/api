c122898e67463de5a5b63ba025d96d6c85036eca
Cody Partington <codythatsme@gmail.com>
2022-07-21 02:51:11 +1000

    moretroubleshooting

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const app = express();
 const dotenv = require("dotenv");
 const cors = require("cors");
 const bodyParser = require("body-parser"); 
+app.use(cors());
 app.use(bodyParser.json({ limit: "5mb" }));
 app.use(bodyParser.urlencoded({ limit: "5mb", extended: true }));
 dotenv.config();
 const apiV1Routes = require("./routes");
 app.use(express.json());
-app.use(cors());
 
 
 app.get("/api/v1", (req, res) => res.send({ info: "Pc Builder API" }));
